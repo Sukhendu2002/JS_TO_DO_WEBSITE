d2fcd17fc8b2d0ae1ec1b46d73ba610e38693a1b
sukhendu2002 <sukhenduguria2017@gmail.com>
2021-03-19 20:26:25 +0530

    Finish the work

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-$("li").click(function(){
+$("ul").on("click","li",function(){
     $(this).toggleClass("apply");
 });
-$("span").click(function (e) { 
+$("ul").on("click","span",function (e) { 
     e.stopPropagation();
     $(this).parent("li").fadeOut(500,()=> {
         $(this).remove();
@@ -9,8 +9,14 @@ $("span").click(function (e) {
 });
 $("input[type='text']").keypress(function (e) { 
     if (e.which === 13) {
-        
-    } else {
-        
-    }
+        var todotext = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span><i class='fas fa-trash'></i></span> "+todotext+"</li>");
+    } 
+});
+$(".fa-plus").click(function(){
+    $("input[type='text']").fadeToggle();
+    $(".rotate").click(function () {
+    $(this).toggleClass("down");
+});
 });
